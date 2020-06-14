aa4cd30f216cd20a3d916de1e310d736f5e9af12
Kosan Nicholas <kosan@shikova.com>
2020-06-14 02:21:11 -0700

    Removes payload internal communication queue Moves Terminal functionality into main.py to allow for terminal-based reloading Removes run.py Updates discord.py version Updates references to discord.py functions to match new API Removes extraneous commands that served too narrow of a scope Adds ability to add commands that are hidden from the help command Fixes some formatting errors to comply further with PEP8 (Incomplete) Adds event for user join to add role; Plans are to roll this into a more comprehensive and configurable auto-role system with help of a future EventActions system, tied in with a persistent database. But this will work for what is needed right this moment. Adjusts requirements.txt to reflect new dependencies

--- bin/main.py
+++ bin/main.py
@@ -2,44 +2,67 @@ import asyncio
 import logging
 import discord
 import socket
+import importlib
 
 import concurrent.futures
 
-from parser import Chat, Payload, PayloadType, Commands
-from terminal import Terminal
+import parse
+import EventActions
+# from parse import Chat, PayloadType, Commands
+from terminal import Terminal as TerminalFull
+
 import config
+
+parser = parse.Chat()
+
+class Terminal(TerminalFull):
+    prompt = ""
+    cmdqueue = False
+    file = None
+
+    def do_reload(self, arg):
+        global parser
+        importlib.reload(parse)
+        parser = parse.Chat()
+        print("Parser reloaded")
+
 
 logging.basicConfig(level=logging.ERROR)
 
 client = discord.Client()
 term = Terminal(client)
 
-parser = Chat()
+
+@client.event
+async def on_member_join(member):
+    role = discord.utils.get(member.guild.roles, name="Visitor")
+    await member.add_roles(role)
+
 
 @client.event
 async def on_ready():
-    pass
+    print(f"Logged in as {client.user}")
+
 
 @client.event
 async def on_message(message):
-    if message.author == client.user.name:
+    if message.author == client.user:
         return
     else:
-        payload = await parser.parse(client, message)
+        await parser.parse(client, message)
 
-    if payload.payloadType == PayloadType.CHAT_MESSAGE:
-        await client.send_message(message.channel, payload.response)
 
 async def main(loop):
     try:
-        await client.start(config.token)
+        await client.start(config.TOKEN)
     except socket.timeout:
         loop.create_task(main(loop))
         return
 
 
-executor = concurrent.futures.ThreadPoolExecutor(max_workers=1)
-loop = asyncio.get_event_loop()
-loop.create_task(main(loop))
-loop.run_in_executor(executor, term.cmdloop)
-loop.run_forever()
+if __name__ == "__main__":
+    executor = concurrent.futures.ThreadPoolExecutor(max_workers=1)
+    loop = asyncio.get_event_loop()
+    loop.create_task(main(loop))
+    loop.run_in_executor(executor, term.cmdloop)
+    loop.run_forever()
