ce7fb6e8a9325b5cb82e9fd6846830835586109b
Gstayton <kosan@shikova.com>
2020-06-23 06:41:05 -0700

    Switches to BigInt and adds PK column

--- bin/db.py
+++ bin/db.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, ForeignKey, Integer, String, Boolean
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean, BigInteger
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy import create_engine
@@ -17,9 +17,8 @@ session = Session()
 
 class Server(Base):
     __tablename__ = "servers"
-    id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
-    server_id = Column(Integer, unique=True, nullable=False)
+    server_id = Column(BigInteger, unique=True, nullable=False, primary_key=True)
 
     users = relationship("User", backref='server', lazy='dynamic')
 
@@ -32,8 +31,7 @@ class Server(Base):
 
 class User(Base):
     __tablename__ = "users"
-    id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, nullable=False, unique=True)
+    user_id = Column(BigInteger, nullable=False, unique=True, primary_key=True)
     username = Column(String(250))
     role_set = Column(Boolean, default=True)
 
