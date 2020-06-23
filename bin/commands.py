dad6ab73e07da1ef64fa89c0a6c2089021e7761c
Gstayton <kosan@shikova.com>
2020-06-23 10:41:13 -0700

    Adds basic user registration persistence
    Contains some currently very test-only commands, though none of them should be problematic.

--- bin/commands.py
+++ bin/commands.py
@@ -1,62 +1,85 @@
 from inspect import getmembers, isfunction, getdoc
-from enum import Enum
 import discord
-from decimal import *
-import random
 
+import db
 
-class Payload:
-    def __init__(self, payloadType, response, target=None, status=0):
-        self.__dict__.update({
-            'Type': payloadType,
-            'Response': response,
-            'Target': target,
-            'Status': status
-        })
-        self.status = status
-        self.payloadType = payloadType
-        self.response = response
-        self.target = target
-
-
-class PayloadType(Enum):
-    CHAT_MESSAGE = 'chatmsg'
-    SYS_MESSAGE  = 'sysmsg'
-    NONE         = 'none'
 
 
 class Commands:
     @staticmethod
     async def ping(client, cmd, args, message):
-        "!Usage: {cmdChar}ping\nUsed to check if alive."
+        """!Usage: {cmdChar}ping\nUsed to check if alive."""
         await message.channel.send(f"pong {args}")
         return
+
+    @staticmethod
+    async def playerid(client: discord.Client, cmd, args, message: discord.Message):
+        """Register new playerid"""
+        if args == "":
+            await message.channel.send("Request must include a playerid")
+            return
+
+        q = db.get_session().query(db.PSO2User.user_id).filter(db.PSO2User.user_id==message.author.id)
+        if db.session.query(q.exists()).scalar():
+            await message.channel.send("User already registered")  # TODO: Add method to update user information
+            return
+        print(message.author.display_name)
+        db.get_session().add(db.PSO2User(
+            user_id=message.author.id,
+            username=f"{message.author.name}#{message.author.discriminator}",
+            player_id=args
+        ))
+        db.get_session().commit()
+
+    @staticmethod
+    async def query_playerid(client, cmd, args, message: discord.Message):
+        """!nope"""
+        s = db.get_session()
+        r = s.query(db.PSO2User).filter_by(player_id=args).first()
+        print(r)
+        await message.channel.send(r)
+
+    @staticmethod
+    async def get_nonmembers(client: discord.Client, cmd, args, message: discord.Message):
+        """!Return users with a registered playerid, but still in the visitor role"""
+        s = db.get_session()
+        members = message.guild.members
+        users = []
+        v_role = discord.utils.get(message.guild.roles, name="Visitor")
+        for member in members:
+            r = s.query(db.PSO2User).filter_by(user_id=member.id).first()
+            if r is None:
+                continue
+            elif v_role in member.roles:
+                users.append(r)
+        await message.channel.send(f"Player_IDs: {[u.player_id for u in users]}")
+
 
     @staticmethod
     async def help(client, cmd, args, message):
         "!Display available commands"
         func_list = [o for o in getmembers(Commands) if isfunction(o[1])]
-        helpText = ""
+        help_text = ""
         if args:
             if " " in args:
-                helpText = "Invalid search"
+                help_text = "Invalid search"
             else:
                 for f in func_list:
                     if args.lower() == f[0] and getdoc(f[1]):
-                        helpText = "Usage for {0}{1}: \n".format(Chat.cmdChar, args)
-                        helpText += getdoc(f[1]).format(cmdChar=Chat.cmdChar)
-                if not helpText:
-                    helpText = "No help available for '{0}'".format(args)
+                        help_text = "Usage for {0}{1}: \n".format(Chat.cmdChar, args)
+                        help_text += getdoc(f[1]).format(cmdChar=Chat.cmdChar)
+                if not help_text:
+                    help_text = "No help available for '{0}'".format(args)
         else:
-            helpText = "Currently implemented commands: \n"
+            help_text = "Currently implemented commands: \n"
             for f in func_list:
                 if getdoc(f[1])[0] == "!":
                     continue
                 else:
-                    helpText += Chat.cmdChar + f[0] + " "
+                    help_text += Chat.cmdChar + f[0] + " "
 
-            helpText += "\nFor more info, try {0}help [command]".format(Chat.cmdChar)
-        await message.channel.send(helpText)
+            help_text += "\nFor more info, try {0}help [command]".format(Chat.cmdChar)
+        await message.channel.send(help_text)
 
 
     @staticmethod
--- bin/db.py
+++ bin/db.py
@@ -1,49 +1,31 @@
-from sqlalchemy import Column, ForeignKey, Integer, String, Boolean
+from sqlalchemy import Column, ForeignKey, VARCHAR, String, Integer
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
 
 import config
 
-Session = sessionmaker()
+S = sessionmaker()
 
 Base = declarative_base()
 
 engine = create_engine(config.SQLALCHEMY_DATABASE_URI)
-Session.configure(bind=engine)
+S.configure(bind=engine)
 
-session = Session()
+session = S()
+def get_session() -> Session:
+    return session
 
+class PSO2User(Base):
+    __tablename__ = "PSO2Users"
 
-class Server(Base):
-    __tablename__ = "servers"
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
-    server_id = Column(Integer, unique=True, nullable=False)
-
-    users = relationship("User", backref='server', lazy='dynamic')
-
-    def __repr__(self):
-        return "Server id: {} / Server Name: {}".format(
-            self.server_id,
-            self.name
-        )
-
-
-class User(Base):
-    __tablename__ = "users"
-    id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, nullable=False, unique=True)
-    username = Column(String(250))
-    role_set = Column(Boolean, default=True)
-
-    server_id = Column(Integer, ForeignKey('servers.id'), nullable=False)
+    user_id = Column(Integer, primary_key=True, nullable=False)
+    username = Column(VARCHAR, primary_key=True, nullable=False)
+    player_id = Column(VARCHAR, primary_key=True, nullable=False)
+    timezone = Column(VARCHAR(64), nullable=True)
 
     def __repr__(self):
-        return "User id: {} / User Name: {}".format(
-            self.user_id,
-            self.username
-        )
-
+        return f"<User(user_id={self.user_id}, username={self.username}, player_id={self.player_id})>"
 
 Base.metadata.create_all(engine)
